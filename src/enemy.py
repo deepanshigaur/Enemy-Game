7f862c15cafb397f77ff080bb3a9911636ab438a
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-11-19 20:08:58 -0500

    final commit

--- src/enemy.py
+++ src/enemy.py
@@ -2,23 +2,37 @@ import pygame
 import random
 #model
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, name, x, y, img_file):
-        #initialize all the Sprite functionality
-        pygame.sprite.Sprite.__init__(self)
+	def __init__(self, name, x, y, img_file):
+		'''
+	creates our enemies, sets their position and speen and gives them name
+	args: self(class), name(self) ,x ,y, img_file
+	return: None
+    		'''
 
-        #The following two attributes must be called image and rect
-        #pygame assumes you have intitialized these values
-        #and uses them to update the screen
+		#initialize all the Sprite functionality
+		pygame.sprite.Sprite.__init__(self)
 
-        #create surface object image
-        self.image = pygame.image.load(img_file).convert_alpha()
-        #get the rectangle for positioning
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        #set other attributes
-        self.name = name + str(id(self))
-        self.speed = 2
+		#The following two attributes must be called image and rect
+		#pygame assumes you have intitialized these values
+		#and uses them to update the screen
 
-    def update(self):
-        print("'Update me,' says " + self.name)
+		#create surface object image
+		self.image = pygame.image.load(img_file).convert_alpha()
+		#get the rectangle for positioning
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		#set other attributes
+		self.name = name + str(id(self))
+		self.speed = 2
+      
+
+	def update(self):
+		'''
+	makes the enemies randomly move on the screen
+	args: self(class)
+	return: None
+		'''
+		self.rect.x += random.randrange (-1,2)
+		self.rect.y += random.randrange (-1,2)
+     
--- src/hero.py
+++ src/hero.py
@@ -2,40 +2,72 @@ import pygame
 import random
 #model
 class Hero(pygame.sprite.Sprite):
-    def __init__(self, name, x, y, img_file):
-        #initialize all the Sprite functionality
-        pygame.sprite.Sprite.__init__(self)
+	def __init__(self, name, x, y, img_file):
+		'''
+    	creates the hero and gives it a name, speed and health
+    	args: self(class), name(class), x, y, img_file
+    	return: None
+    		'''
+		#initialize all the Sprite functionality
+		pygame.sprite.Sprite.__init__(self)
+		#The following two attributes must be called image and rect
+		#pygame assumes you have intitialized these values
+		#and uses them to update the screen
 
-        #The following two attributes must be called image and rect
-        #pygame assumes you have intitialized these values
-        #and uses them to update the screen
+		#create surface object image
+		self.image = pygame.image.load(img_file).convert_alpha()
+        	#get the rectangle for positioning
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		#set other attributes
+		self.name = name
+		self.speed = 3
+		self.health = 3
 
-        #create surface object image
-        self.image = pygame.image.load(img_file).convert_alpha()
-        #get the rectangle for positioning
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        #set other attributes
-        self.name = name
-        self.speed = 3
-        self.health = 3
+		#methods to make moving our hero easier
+	def move_up(self):
+		'''
+	moves the hero up
+	args: self(class)
+	return: None
+		'''
+		self.rect.y -= self.speed
 
-    #methods to make moving our hero easier
-    def move_up(self):
-        self.rect.y -= self.speed
-    def move_down(self):
-        self.rect.y += self.speed
-    def move_left(self):
-        self.rect.x -= self.speed
-    def move_right(self):
-        self.rect.x += self.speed
+	def move_down(self):
+		'''
+    	moves the hero down
+    	args: self(class)
+    	return: None
+    		'''
+		self.rect.y += self.speed
 
-    def fight(self, opponent):
-        if(random.randrange(3)):
-            self.health -= 1
-            print("attack failed. Remaining Health: ", self.health)
-            return False
-        else:
-            print("successful attack")
-        return True
+	def move_left(self):
+		'''
+    	moves the hero left
+   	args: self(class)
+    	return: None
+    		'''
+		self.rect.x -= self.speed
+
+	def move_right(self):
+		'''
+	moves the hero right
+	args: self(class)
+	return: None
+		'''
+		self.rect.x += self.speed
+
+	def fight(self, opponent):
+		'''
+	makes the  hero and enemy fights and reduces the health fof the hero if the attacks fails
+	args: self(class), opponent
+	return: True(bool) and False(bool)
+    		'''
+		if(random.randrange(3)):
+			self.health -= 1
+			print("attack failed. Remaining Health: ", self.health)
+			return False
+		else:
+			print("successful attack")
+		return True
